4f31491430042a3d813a8f7fa20e32d18eac8979
vishal jayapalan <vishal935@gmail.com>
2020-12-10 13:42:26 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 5000
+const uri = process.env.DATABASE_URI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+mongoose.connection
+  .once('open', () => {
+    console.log('MongoDB connection established succesfully...')
+  })
+  .on('error', error => {
+    console.log('Connection Error:', error)
+  })
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
